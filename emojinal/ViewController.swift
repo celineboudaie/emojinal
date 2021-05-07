d799b95ba911d177113490914a113adbbc95dbdb
celine <cboudaie@gmail.com>
2021-05-06 23:03:25 -0700

    Initial Commit

---
+++ emojinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojinal
+//
+//  Created by Celine Boudaie on 5/6/21.
+//  Copyright © 2021 Celine Boudaie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
