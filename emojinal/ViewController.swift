1fb058b7c40ade0b8d5c45c783c1928648a2bb95
celine <cboudaie@gmail.com>
2021-05-07 01:24:50 -0700

    final changes

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2021 Celine Boudaie. All rights reserved.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,13 +16,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+     let emojis = ["🥳": "party time!!", "🙃": "u good?"]
    
+    let customMessages = ["party time!!": ["facetime Karlie", "make perfect 10 cookies"], "u good?": ["cheer up", "press the party emoji"]]
+    
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "How are you feeling today?", message: "hi", preferredStyle: UIAlertController.Style.alert)
         
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let selectedEmotion = (sender.titleLabel?.text)!
+        let options = customMessages[emojis[selectedEmotion]!]!
         
-        present(alertController, animated: true, completion: nil)
+        let emojiMessage = options[0]
+        
+        let alertController = UIAlertController(title: "My recommendation:", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+               
+               alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+               
+               present(alertController, animated: true, completion: nil)
     }
 }
-
